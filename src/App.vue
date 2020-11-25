d270afd57aeb40feaea2f796de04d3f14445c819
Dana Varahi <dana_varahi@protonmail.com>
2020-11-25 16:55:56 +0000

    Finshed MVP: added country detail

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="container">
+  <div>
     <h1>Countries</h1>
-    <countires-list :countries='countries'></countires-list>
+    <div class="container">
+      <countires-list :countries='countries'></countires-list>
+      <country-detail :country="selectedCountry"></country-detail>
+     </div>
   </div>
 </template>
 
 <script>
 import CountriesList from './components/CountriesList.vue'
 import { eventBus } from './main.js';
+import CountryDetail from './components/CountryDetail.vue'
 
 export default {
   name: 'app',
@@ -24,19 +28,24 @@ export default {
     .then(countries => this.countries = countries)
 
     eventBus.$on('selected-country', (country) => {
+      console.log(country)
       this.selectedCountry = country;
     })
 
   },
   components: {
     "countires-list": CountriesList,
+    "country-detail": CountryDetail
   }
 }
 
 </script>
 
 <style scoped>
-
+  .container {
+    display: flex;
+    justify-content: space-between;
+  }
 </style>
 
 
---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-if="country">
+         <h2>{{ country.name}}</h2>
+         <p>{{ country.capital }}</p>
+         <p>{{ country.population }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "country-detail",
+    props: ['country']
+}
+</script>
+
+<style scoped>
+
+</style>
